288b686ad392b0f058fc246d41f1a373677fe39e
willsgimenes <willsgimenes@gmail.com>
2020-08-04 21:04:02 -0300

    feat(app): adding component highlighter to home

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,42 +1,57 @@
 import Head from 'next/head'
-import { getQuote } from "@willsgimenes/marina";
-import { getQuote as PlasticLove } from "@willsgimenes/plastic-love";
+import {getQuote} from "@willsgimenes/marina";
+import {getQuote as PlasticLove} from "@willsgimenes/plastic-love";
+
+import ComponentHighlighter from "@willsgimenes/cosmo-clock-21";
 
 const Home = () => (
-  <div className="container">
-    <Head>
-      <title>City pop</title>
-      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎸</text></svg>"/>
-    </Head>
+    <div className="container">
+        <Head>
+            <title>City pop</title>
+            <link rel="icon"
+                  href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎸</text></svg>"/>
+        </Head>
 
-    <main
-        style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100vh',
-            textAlign: 'center'
-        }}
-    >
-      <h1 className="title">
-        You should listen city pop! 🎸
-      </h1>
+        <main
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh',
+                textAlign: 'center'
+            }}
+        >
+            <h1 className="title">
+                You should listen city pop! 🎸
+            </h1>
 
-      <section>
+            <section>
+                <ComponentHighlighter
+                    active={true}
+                    fullScopeName
+                    data-package-id="@willsgimenes/marina"
+                >
+                    <p>{getQuote()}</p>
+                </ComponentHighlighter>
 
-          <p>{ getQuote() }</p>
+                <ComponentHighlighter
+                    active={true}
+                    fullScopeName
+                    data-package-id="@willsgimenes/plastic-love"
+                >
+                    <p>{PlasticLove()}</p>
+                </ComponentHighlighter>
 
-          <p>{ PlasticLove() }</p>
+                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/hgb2Omu6voI"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen></iframe>
+            </section>
 
-        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/hgb2Omu6voI" frameBorder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen></iframe>
-      </section>
+        </main>
 
-    </main>
-
-  </div>
+    </div>
 )
 
 export default Home
